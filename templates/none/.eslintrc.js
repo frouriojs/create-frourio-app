38378417baf360a032f2eba03f0a15c2b25b87ac
m-mitsuhide <genbu2011@live.jp>
2020-06-08 13:03:15 +0900

    chore: add none template

---
+++ templates/none/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+  }
+}
---
+++ templates/none/server/frourio.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  port: 8080,
+  basePath: '/api',
+  helmet: true,
+  dotenv: false,
+  cors: true
+}
