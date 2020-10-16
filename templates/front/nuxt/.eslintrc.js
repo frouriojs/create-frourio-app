2cc94ebc0a42e8827d1bbd952100605e2e2b2155
solufa <solufa2020@gmail.com>
2020-10-17 00:43:36 +0900

    fix: typecheck and lint

--- templates/front/nuxt/.eslintrc.js
+++ templates/front/nuxt/.eslintrc.js
@@ -11,10 +11,7 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
-  rules: {
-  }
+  rules: {}
 }
