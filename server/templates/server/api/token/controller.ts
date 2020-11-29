e5055eecbf5257bf808e6749f1863de82ecec8ea
solufa <solufa2020@gmail.com>
2020-11-30 01:22:37 +0900

    feat: change passport to express-jwt

--- server/templates/server/api/token/controller.ts
+++
@@ -1,9 +0,0 @@
-import { defineController } from './$relay'
-import { validateUser } from '$/service/user'
-
-export default defineController((fastify) => ({
-  post: ({ body }) =>
-    validateUser(body.id, body.pass)
-      ? { status: 201, body: { token: fastify.jwt.sign({ id: body.id }) } }
-      : { status: 401 }
-}))
