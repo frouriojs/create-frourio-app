8be19c0bade203c0d1551df51e7dcf894c878d3b
solufa <solufa2020@gmail.com>
2020-12-11 23:24:47 +0900

    fix(sapper): add node-fetch

--- server/templates/client/sapper/src/utils/apiClient.ts
+++ server/templates/client/sapper/src/utils/apiClient.ts
@@ -1,4 +1,6 @@
 import aspida from '@aspida/<%= aspida %>'
 import api from '~/server/api/$api'
 
-export const apiClient = api(aspida())
+export const apiClient = <% if (aspida === 'axios') { %>api(aspida())<% } else { %>(process as { browser?: boolean }).browser
+  ? api(aspida(fetch))
+  : api(require('@aspida/node-fetch').default(require('node-fetch')))<% } %>
