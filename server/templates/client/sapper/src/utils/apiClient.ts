e98f8ae7d24fc09f35e559e577222038a640ea69
solufa <solufa2020@gmail.com>
2020-12-15 19:45:04 +0900

    fix(sapper): err of importing @aspida/axios

--- server/templates/client/sapper/src/utils/apiClient.ts
+++ server/templates/client/sapper/src/utils/apiClient.ts
@@ -1,6 +1,4 @@
 import aspida from '@aspida/<%= aspida %>'
 import api from '~/server/api/$api'
 
-export const apiClient = <% if (aspida === 'axios') { %>api(aspida())<% } else { %>(process as { browser?: boolean }).browser
-  ? api((aspida as any).default(fetch))
-  : api(require('@aspida/node-fetch').default(require('node-fetch')))<% } %>
+export const apiClient = api((process as { browser?: boolean }).browser ? (aspida as any).default() : <% if (aspida === 'axios') { %>aspida()<% } else { %>require('@aspida/node-fetch').default(require('node-fetch'))<% } %>)
