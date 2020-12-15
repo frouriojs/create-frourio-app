76fe21154fbb21a3b8bc063f9a206226e6445bd3
solufa <solufa2020@gmail.com>
2020-12-15 10:42:44 +0900

    fix: check db connection with all answers

--- server/api/dbConnection/controller.ts
+++ server/api/dbConnection/controller.ts
@@ -1,25 +1,29 @@
 import mariadb from 'mariadb'
 import { Client } from 'pg'
+import { genAllAnswers } from '$/service/answers'
 import { defineController } from './$relay'
 
 export default defineController(() => ({
   post: async ({ body }) => {
-    const db = body.orm === 'prisma' ? body.prismaDB : body.typeormDB
-    if (!db || db === 'sqlite') return { status: 200, body: { enabled: true } }
+    const allAnswers = genAllAnswers(body)
+
+    if (allAnswers.orm === 'none' || allAnswers.prismaDB === 'sqlite') {
+      return { status: 200, body: { enabled: true } }
+    }
 
     try {
       const config = {
-        host: body.dbHost,
-        port: +`${body.dbPort}`,
-        user: body.dbUser,
-        password: body.dbPass
+        host: allAnswers.dbHost,
+        port: +`${allAnswers.dbPort}`,
+        user: allAnswers.dbUser,
+        password: allAnswers.dbPass
       }
 
-      if (db === 'mysql') {
+      if ((allAnswers.prismaDB ?? allAnswers.typeormDB) === 'mysql') {
         const conn = await mariadb.createConnection(config)
 
-        if (body.orm === 'typeorm') {
-          await conn.query(`CREATE DATABASE IF NOT EXISTS ${body.dbName}`)
+        if (allAnswers.orm === 'typeorm') {
+          await conn.query(`CREATE DATABASE IF NOT EXISTS ${allAnswers.dbName}`)
         }
 
         await conn.end()
@@ -27,11 +31,11 @@ export default defineController(() => ({
         const client = new Client({ ...config, database: 'postgres' })
         await client.connect()
 
-        if (body.orm === 'typeorm') {
+        if (allAnswers.orm === 'typeorm') {
           const res = await client.query('SELECT datname FROM pg_database')
 
-          if (res.rows.every((r) => r.datname !== body.dbName)) {
-            await client.query(`CREATE DATABASE ${body.dbName}`)
+          if (res.rows.every((r) => r.datname !== allAnswers.dbName)) {
+            await client.query(`CREATE DATABASE ${allAnswers.dbName}`)
           }
         }
 
