4300c5cd17fa0d3f68528cb46a84c565a488e0fa
Luma <world@luma.email>
2021-02-04 15:29:00 +0900

    fix: template

--- server/templates/server/@server=fastify/entrypoints/@orm=prisma/index.ts
+++ server/templates/server/@server=fastify/entrypoints/@orm=prisma/index.ts
@@ -3,12 +3,12 @@ import { API_SERVER_PORT } from '$/service/envValues'
 
 <% if (daemon === 'pm2') { %>
 init()
-  .listen(SERVER_PORT)
+  .listen(API_SERVER_PORT)
   .then(() => {
     // PM2 graceful start
     // See also https://pm2.keymetrics.io/docs/usage/signals-clean-restart/
     process.send?.('ready')
   })
 <% } else { %>
-init().listen(SERVER_PORT)
+init().listen(API_SERVER_PORT)
 <% } %>
