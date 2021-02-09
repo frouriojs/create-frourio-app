fae57b48f52cf303d57570f887fd9710ca65a292
Luma <world@luma.email>
2021-02-09 10:21:07 +0900

    feat: example.env and new env TEST_CFA_FIX_CLIENT

--- server/utils/answers/random.ts
+++ server/utils/answers/random.ts
@@ -34,6 +34,8 @@ export const createRandomAnswers = async (
   if (ans.orm === 'typeorm' && ans.db === 'sqlite') {
     ans.db = 'mysql'
   }
+  if (process.env.TEST_CFA_FIX_CLIENT)
+    ans.client = process.env.TEST_CFA_FIX_CLIENT
   if (process.env.TEST_CFA_FIX_DB) ans.db = process.env.TEST_CFA_FIX_DB
   if (process.env.TEST_CFA_FIX_ORM) ans.orm = process.env.TEST_CFA_FIX_ORM
   if (ans.db === 'sqlite') {
