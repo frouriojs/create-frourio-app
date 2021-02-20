b5e46f8319ecb708d20aeb4d4baef91160ca7a9e
Luma <world@luma.email>
2021-02-20 15:33:10 +0900

    chore(template): Bind to 0.0.0.0 by default in fastify to make setup easy.

--- server/templates/server/@server=fastify/entrypoints/@orm=prisma/index.ts
+++ server/templates/server/@server=fastify/entrypoints/@orm=prisma/index.ts
@@ -3,12 +3,12 @@ import { API_SERVER_PORT } from '$/service/envValues'
 
 <% if (daemon === 'pm2') { %>
 init()
-  .listen(API_SERVER_PORT)
+  .listen(API_SERVER_PORT, '0.0.0.0')
   .then(() => {
     // PM2 graceful start
     // See also https://pm2.keymetrics.io/docs/usage/signals-clean-restart/
     process.send?.('ready')
   })
 <% } else { %>
-init().listen(API_SERVER_PORT)
+init().listen(API_SERVER_PORT, '0.0.0.0')
 <% } %>
