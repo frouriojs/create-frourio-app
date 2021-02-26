5da6555c767b0eb20729aa2bf07e7da684897f3a
Luma <world@luma.email>
2021-02-26 17:48:13 +0900

    test: capitalize

---
+++ server/utils/string/__tests__/index.spec.ts
@@ -0,0 +1,8 @@
+import {capitailze} from ".."
+
+describe("capitailze", () => {
+  it("capitalize an alphabetical word", () => {
+    expect(capitailze('abc')).toBe('Abc')
+    expect(capitailze('XYZ')).toBe('XYZ')
+  })
+})
