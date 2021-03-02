492c72ed4b54acf83a12e997364620b6118ee7c1
Luma <world@luma.email>
2021-03-02 22:09:08 +0900

    fix: open from server-side

--- server/api/answers/controller.ts
+++ server/api/answers/controller.ts
@@ -3,7 +3,8 @@ import { getAnswers, updateAnswers } from '$/service/answers'
 import { getStatus } from '$/service/status'
 import stream from 'stream'
 import axios from 'axios'
-import { getServerPort } from '$/service/getServerPort'
+import { getClientPort, getServerPort } from '$/service/getServerPort'
+import open from 'open'
 
 export default defineController(({ appendLogging, clientReady }) => ({
   get: () => ({ status: 200, body: getAnswers() }),
@@ -16,8 +17,9 @@ export default defineController(({ appendLogging, clientReady }) => ({
       }
     })
     updateAnswers(body, s).then(async () => {
+      const clientPort = await getClientPort()
       const serverPort = await getServerPort()
-      for (let i = 0; i < 100; i++) {
+      for (let i = 0; i < 600; i++) {
         await new Promise((resolve) => setTimeout(resolve, 1000))
         try {
           const client = axios.create({
@@ -26,6 +28,13 @@ export default defineController(({ appendLogging, clientReady }) => ({
           const res = await client.get('tasks')
           if (res.status === 200) {
             clientReady()
+            const subprocess = await open(`http://localhost:${clientPort}`)
+            subprocess.on('error', () => {
+              console.log(`open http://localhost:${clientPort} in the browser`)
+            })
+            subprocess.on('close', () => {
+              console.log(`open http://localhost:${clientPort} in the browser`)
+            })
             break
           }
         } catch (e: unknown) {
