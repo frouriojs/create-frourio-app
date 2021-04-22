fa5d02ff70be97d95043ba47fd88f942b6dd3257
solufa <solufa2020@gmail.com>
2021-04-23 01:19:23 +0900

    feat: remove eslint-plugin-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:prettier/recommended',
     'prettier'
   ],
   plugins: ['@typescript-eslint', 'react'],
--- server/templates/@client=nuxt/.eslintrc.js
+++ server/templates/@client=nuxt/.eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
     'prettier'
   ],
