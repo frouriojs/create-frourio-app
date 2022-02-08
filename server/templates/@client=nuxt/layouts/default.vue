11dd9bca1967568ddeed312311adacee56e9896e
Luma <world@luma.email>
2022-02-09 01:22:02 +0900

    upgrade stylelint to v14 and add postcss-html to fix errors

--- server/templates/@client=nuxt/layouts/default.vue
+++ server/templates/@client=nuxt/layouts/default.vue
@@ -16,8 +16,7 @@ html {
     Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
+  text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
