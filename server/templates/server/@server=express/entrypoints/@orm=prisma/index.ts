309421d07363b23bac1642a3b3535c33cbfffcd7
Luma <world@luma.email>
2022-02-10 08:28:03 +0900

    feat: update aws-serverless-experss to @vendia/serverless-express and switching lambda support from fastify to express
    resolves https://github.com/frouriojs/create-frourio-app/issues/230

--- server/templates/server/@server=express/entrypoints/@orm=prisma/index.ts
+++ server/templates/server/@server=express/entrypoints/@orm=prisma/index.ts
@@ -1,17 +1,7 @@
-import express from 'express'
-import helmet from 'helmet'
-import cors from 'cors'
-import server from '$/$server'
+import { init } from "$/service/app";
 import {
   API_SERVER_PORT,
-  API_BASE_PATH,
-  API_UPLOAD_DIR
 } from '$/service/envValues'
 
-const app = express()
-app.use(helmet())
-app.use(cors())
-server(app, { basePath: API_BASE_PATH })
-app.use('/static', express.static('static'))
-if (API_UPLOAD_DIR) app.use('/upload', express.static(API_UPLOAD_DIR))
+const app = init();
 app.listen(API_SERVER_PORT)
