bb3a998b097966083fb7bbdf44d953be2ab932a7
Luma <world@luma.email>
2022-02-10 08:22:21 +0900

    feat: update baseUrl to connect the same host in development
    resolves https://github.com/frouriojs/create-frourio-app/issues/266

--- server/templates/@client=next/aspida.config.js
+++ server/templates/@client=next/aspida.config.js
@@ -2,5 +2,5 @@ require('dotenv').config({ path: 'server/.env' })
 
 module.exports = {
   input: 'server/api',
-  baseURL: `${process.env.API_ORIGIN || ''}${process.env.API_BASE_PATH || ''}`
+  baseURL: process.env.NODE_ENV === 'production' ? `${process.env.API_ORIGIN || ''}${process.env.API_BASE_PATH || ''}` : `${window.location.protocol}//${window.location.hostname}:${process.env.API_PORT}${process.env.API_BASE_PATH || ''}`,
 }
