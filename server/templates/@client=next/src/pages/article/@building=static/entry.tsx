2ddd72106d8f530afa082031c5a5da6e6a3b821e
Luma <world@luma.email>
2022-02-21 18:42:53 +0900

    support other than react-query

--- server/templates/@client=next/src/pages/article/@building=static/entry.tsx
+++ server/templates/@client=next/src/pages/article/@building=static/entry.tsx
@@ -1,10 +1,12 @@
-import { useAspidaQuery } from '@aspida/react-query'
+<% if (reactHooks === 'none') { %>import { useState, useEffect } from 'react'<% } else if (reactHooks === 'swr') { %>
+import useAspidaSWR from '@aspida/swr'<% } else if (reactHooks === 'query') { %>
+import { useAspidaQuery } from '@aspida/react-query'<% } %>
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
-import React from 'react'
 import Layout from '~/components/Layout'
 import { apiClient } from '~/utils/apiClient'
+<% if (reactHooks === 'none') { %>import type { ArticleInfo } from '$/service/article'<% } %>
 
 export type Query = {
   id: number
@@ -12,9 +14,16 @@ export type Query = {
 
 const Article: NextPage = () => {
   const router = useRouter()
+  <% if (reactHooks === 'none') { %>const [article, setArticle] = useState<ArticleInfo | null>(null)
+  useEffect(() => {
+    apiClient.article._articleId(Number.parseInt(router.query.id as string, 10)).$get().then((article) => {setArticle(article)})
+  }, [router.query.id])<% } else if (reactHooks === 'swr') { %>
+  const { data: article } = useAspidaSWR(
+    apiClient.article._articleId(Number.parseInt(router.query.id as string, 10))
+  )<% } else if (reactHooks === 'query') { %>
   const { data: article } = useAspidaQuery(
     apiClient.article._articleId(Number.parseInt(router.query.id as string, 10))
-  )
+  )<% } %>
   return (
     <Layout>
       <Head>
