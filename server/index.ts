c4474e27f96d32c1243cf4ec2a5c2c4f5a4d5383
JunIshino <jun.ishino@kepple.jp>
2022-02-27 21:36:12 +0900

    Add ts-ignore

--- server/index.ts
+++ server/index.ts
@@ -8,6 +8,8 @@ import { cliMigration, updateAnswers } from '$/service/answers'
 import FastifyWebsocket from 'fastify-websocket'
 import FastifyInject from './plugins/fastify-inject'
 import stream from 'stream'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import metaInfo from '../package.json'
 import { Command } from 'commander'
 
