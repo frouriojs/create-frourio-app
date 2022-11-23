5f4adf63e88da84a9da4d98632987b16bea53646
solufa <solufa2020@gmail.com>
2022-11-23 16:03:07 +0900

    feat: update express-jwt v7

--- server/templates/server/@server=express/api/user/hooks.ts
+++ server/templates/server/@server=express/api/user/hooks.ts
@@ -1,13 +1,13 @@
-import jwt from 'express-jwt'
+import { expressjwt } from 'express-jwt'
 import { defineHooks } from './$relay'
 import { API_JWT_SECRET } from '$/service/envValues'
 
 export type AdditionalRequest = {
-  user: {
+  auth: {
     id: string
   }
 }
 
 export default defineHooks(() => ({
-  onRequest: jwt({ secret: API_JWT_SECRET, algorithms: ['HS256'] })
+  onRequest: expressjwt({ secret: API_JWT_SECRET, algorithms: ['HS256'] })
 }))
