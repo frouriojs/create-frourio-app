b1043e7aa7906740c574b12e404f24c312297da2
solufa <solufa2020@gmail.com>
2025-02-01 11:17:36 +0900

    feat: remove git init process

--- server/service/completed.ts
+++ server/service/completed.ts
@@ -4,7 +4,6 @@ import { spawn } from 'child_process'
 import chalk from 'chalk'
 import { Answers } from '$/common/prompts'
 import stream from 'stream'
-import fs from 'fs'
 import realExecutablePath from 'real-executable-path'
 
 export const npmInstall = async (cwd: string, npmClient: string, s: stream.Writable) => {
@@ -34,21 +33,7 @@ export const npmInstall = async (cwd: string, npmClient: string, s: stream.Writa
 export const completed = async (answers: Answers, s: stream.Writable) => {
   const outDir = path.resolve(answers.dir ?? './new-frourio-app')
   const npmClientPath = await realExecutablePath('npm')
-  const gitCliPath = await realExecutablePath('git')
 
-  await new Promise((resolve, reject) => {
-    const proc = spawn(gitCliPath, ['init'], {
-      stdio: ['inherit', 'pipe', 'pipe'],
-      cwd: outDir,
-      shell: process.platform === 'win32'
-    })
-    proc.stdio[1]?.on('data', s.write.bind(s))
-    proc.stdio[2]?.on('data', s.write.bind(s))
-    proc.once('close', resolve)
-    proc.once('error', reject)
-  })
-
-  await fs.promises.writeFile(path.resolve(outDir, '.git/HEAD'), 'ref: refs/heads/main')
   await npmInstall(outDir, npmClientPath, s)
   await npmInstall(path.resolve(outDir, 'server'), npmClientPath, s)
 
